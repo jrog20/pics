514e596c76914b58208fc20a1d4035d6a103e1f4
jrog20 <jaimerogstad@gmail.com>
2021-10-28 20:14:06 -0400

    Add async await

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
-
-  onSearchSubmit(term) {
+  // async/await syntax - replaces .then
+  async onSearchSubmit(term) {
     // axios.get takes two parameters ->
     // 1. the URI of the service endpoint
     // 2. an object that contains the properties we want to send to the server
-    axios.get('https://api.unsplash.com/search/photos', {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
       // Unsplash param query = Search terms.
       // Adding params will add ?query=searchterm to end of URI
       params: { query: term },
@@ -17,6 +17,7 @@ class App extends React.Component {
         Authorization: 'Client-ID vXO8t_QybpDh73rhxROhnaVxg2bm11h14VGHI8stS1U'
       }
     })
+    console.log(response.data.results)
   }
 
   render() {
