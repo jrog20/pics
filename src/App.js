abf34d60a0828d17f3bb8881159ac456bc7c283e
jrog20 <jaimerogstad@gmail.com>
2021-10-29 14:43:33 -0400

    Add state and refactor to arrow function

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 
 class App extends React.Component {
+  state = {
+    images: []
+  }
+
   // async/await syntax - replaces .then
-  async onSearchSubmit(term) {
+  // refactor into arrow function to avoid binding this to onSubmit prop
+  onSearchSubmit = async (term) => {
     // axios.get takes two parameters ->
     // 1. the URI of the service endpoint
     // 2. an object that contains the properties we want to send to the server
@@ -17,13 +22,15 @@ class App extends React.Component {
         Authorization: 'Client-ID vXO8t_QybpDh73rhxROhnaVxg2bm11h14VGHI8stS1U'
       }
     })
-    console.log(response.data.results)
+    console.log(this)
+    this.setState({ images: response.data.results })
   }
 
   render() {
     return (
       <div className="ui container margin">
         <SearchBar onSubmit={this.onSearchSubmit}/>
+        Found: {this.state.images.length} images
       </div>
     )
   }
